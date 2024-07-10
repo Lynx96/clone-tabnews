cb1ff5320f78138a9066c999a149f83dbfcd817a
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2024-07-10 19:02:56 +0000

    add database module and  file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query('SELECT 3 + 3 as Sum;');
+  console.log(result.rows);
   response.status(200).json({chave: "PRAAFTzão"})
 }
 
-export default status;
+export default status;  
