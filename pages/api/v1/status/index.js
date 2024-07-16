bb7aa6279fadac6baa8e659aea5b36e4ed2ca8c5
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2024-07-16 16:19:02 +0000

    Finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,22 +2,31 @@ import database from "infra/database.js"
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
+
   const psqlVersion = await database.Query('SHOW server_version;');
-  const version = psqlVersion.rows[0].server_version;  
-  console.log(version)
-  const usedConnections = await database.Query("SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db'")
-  console.log(usedConnections.rows[0].count)
-  const maxConnections = await database.Query("SELECT name, setting FROM pg_settings WHERE name = 'max_connections'")
-  console.log(maxConnections.rows[0].setting)
+  const psqlVersionResult = psqlVersion.rows[0].server_version;  
+
+  const maxConnections = await database.Query("SHOW max_connections;")
+  const maxConnectionsResult = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  
+  const usedConnections = await database.Query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+    })
+  const usedConnectionsResult = usedConnections.rows[0].count;  
+ 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
-        psql_version: version
+        psql_version: psqlVersionResult,
+        max_connections: parseInt(maxConnectionsResult),
+        used_connections: parseInt(usedConnectionsResult),
+
       }
-    },   
-    used_connections: parseInt(usedConnections.rows[0].count),
-    max_connections: parseInt(maxConnections.rows[0].setting)
+    }   
   })
 
  
