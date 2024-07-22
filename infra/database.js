ecc3ae122e2513b348ac282df6a392ce49b213d7
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2024-07-22 19:21:52 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,14 +6,16 @@ async function query(queryObject){
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD    
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,    
   });
   console.log('Credenciais do banco local:', {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD    
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV,    
   })
   try {
     await client.connect();  
