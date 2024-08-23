18cc5ac10f797aeaca2f07961d188146aeb35d0e
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2024-08-23 17:05:56 +0000

    Fix  endpoint used connections bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,7 +3,16 @@ import {join} from 'node:path'
 import database from 'infra/database.js'
 
 export default async function migrations(request, response) { 
-  const dbClient = await database.GetNewClient();
+  const allowedMethods = ['POST', 'GET'];
+  if(!allowedMethods.includes(request.method)){
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`
+    });
+
+  }
+  let dbClient;
+  try {
+  dbClient = await database.GetNewClient();    
 
   const defaultMigrationRunner = {
     dbClient: dbClient,
@@ -16,7 +25,7 @@ export default async function migrations(request, response) {
   
   if (request.method === 'GET'){
     const pendingMigrations = await migrationRunner(defaultMigrationRunner)
-    await dbClient.end();
+    
     return response.status(200).json(pendingMigrations)  
   }
 
@@ -26,14 +35,23 @@ export default async function migrations(request, response) {
       dryRun: false,
     });
 
-    await dbClient.end();
+   
 
     if (migratedMigrations.length > 0) {
       return  response.status(201).json(migratedMigrations)
     }
     return response.status(200).json(migratedMigrations)  
 
-  } 
+  }  
+    
+  } catch (error) {
+    console.error(error)
+    throw error
+    
+  } finally {
+    await dbClient.end();
 
-  return response.status(405).end();
+  }
+  
+  
 }
