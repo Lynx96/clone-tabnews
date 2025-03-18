e500621c314a15f6b484f564cb383f9be42125ee
Lynx96 <39319226+Lynx96@users.noreply.github.com>
2025-03-18 00:53:21 +0000

    refactor: add default controller handler behaviour for status and migrations endpoints

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,44 +1,39 @@
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import { createRouter } from "next-connect";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
+import controller from "infra/controller.js";
 
-    const psqlVersion = await database.Query("SHOW server_version;");
-    const psqlVersionResult = psqlVersion.rows[0].server_version;
+const router = createRouter();
 
-    const maxConnections = await database.Query("SHOW max_connections;");
-    const maxConnectionsResult = maxConnections.rows[0].max_connections;
+router.get(getHandler);
 
-    const databaseName = process.env.POSTGRES_DB;
+export default router.handler(controller.errorHandlers);
 
-    const usedConnections = await database.Query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [databaseName],
-    });
-    const usedConnectionsResult = usedConnections.rows[0].count;
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          psql_version: psqlVersionResult,
-          max_connections: parseInt(maxConnectionsResult),
-          used_connections: parseInt(usedConnectionsResult),
-        },
+  const psqlVersion = await database.Query("SHOW server_version;");
+  const psqlVersionResult = psqlVersion.rows[0].server_version;
+
+  const maxConnections = await database.Query("SHOW max_connections;");
+  const maxConnectionsResult = maxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const usedConnections = await database.Query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const usedConnectionsResult = usedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        psql_version: psqlVersionResult,
+        max_connections: parseInt(maxConnectionsResult),
+        used_connections: parseInt(usedConnectionsResult),
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller");
-    console.log(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
